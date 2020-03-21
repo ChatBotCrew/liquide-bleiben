545e492409ce7462372798d9690ffe54891e397e
TimMaa <tim.maasen@ymail.com>
2020-03-22 00:31:11 +0100

    Add API definition and Google Spreadsheets

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 var express = require('express');
 var app = express();
+var { GoogleSpreadsheet } = require('google-spreadsheet');
 
-const columnNames = [
-  "Name",
-  "Land",
-  "Fördersumme",
-  "Mitarbeiter",
-  "Jahresabschluss"
+let doc;
+if (process.env.GOOGLE_SHEET_URL && process.env.GOOGLE_API_KEY) {
+  doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_URL);
+  doc.useApiKey(process.env.GOOGLE_API_KEY);
+  
+  (async () => {
+    await doc.loadInfo(); // loads document properties and worksheets
+    console.log(doc.title);
+  })()
+}
+
+
+const columns = [
+  {
+    name: "Name",
+    key: "name",
+  },
+  {
+    name: "Bundesland",
+    key: "land",
+  },
+  {
+    name: "Fördersumme",
+    key: "summe",
+  },
+  {
+    name: "Mitarbeiter",
+    key: "mitarbeiter",
+  },
+  {
+    name: "Jahresabschluss",
+    key: "jahresabschluss",
+  },
+  {
+    name: "Weitere Infos",
+    key: "infolink",
+  },
 ]
 
 const offers = [
@@ -36,7 +68,11 @@ const offers = [
 app.use(express.static('public'));
 
 app.get('/api/offers', (req, res) => {
-  res.send({ columnNames, offers })
+  res.send({ columns, offers })
 });
 
 app.listen(8080, function () { console.log('"Wir bleiben Liquide" running on 8080!'); });
+
+const filterOffers = () => {
+
+}
