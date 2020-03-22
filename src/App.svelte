5eda78437b0405da059bae67823321c73bb686a4
TimMaa <tim.maasen@ymail.com>
2020-03-22 15:00:23 +0100

    Change texts

--- src/App.svelte
+++ src/App.svelte
@@ -52,13 +52,13 @@
   {/if}
   {#if currentStep === 1}
     <div class="fullpage" in:fly={{ x: 1000, duration: 1500 }} out:fly={{ x: -1000, duration: 1500 }}>
-      <Select categoryName="Bundesland" bind:value={selection.state} options={bundeslaender} help="Hiermit können wir Dir helfen die Programme aus deinem Bundesland für Dich zu finden. Bitte wähle das Bundesland aus in dem der Sitz Deiner Betriebsstätte ist." />
+      <Select categoryName="Bundesland" bind:value={selection.state} options={bundeslaender} help="Hiermit können wir Ihnen helfen die Programme aus Ihrem Bundesland für Sie zu finden. Bitte wählen Sie das Bundesland aus, in dem der Sitz Ihrer Betriebsstätte ist." />
     </div>
     <button disabled={!selection.state} transition:fade|local class="next" on:click={next}>Weiter</button>
   {/if}
   {#if currentStep === 2}
     <div class="fullpage" in:fly={{ x: 1000, duration: 1500 }} out:fly={{ x: -1000, duration: 1500 }}>
-      <Select categoryName="Gewerbe" bind:value={selection.trade} options={gewerbe} help="Für einige Branchen gibt es spezielle Förder- und Hilfsprogramme, lass uns wissen in welcher Branche Du tätig bist damit wir Dir genauere Vorschläge machen können." />
+      <Select categoryName="Gewerbe" bind:value={selection.trade} options={gewerbe} help="Für einige Branchen gibt es spezielle Förder- und Hilfsprogramme. Lassen Sie uns wissen in welcher Branche Sie tätig sind, damit wir Ihnen genauere Vorschläge machen können." />
     </div>
     <button disabled={!selection.trade} transition:fade|local class="next" on:click={next}>Weiter</button>
   {/if}
@@ -70,14 +70,14 @@
         Jahre alt
       </div>
       <div class="help-text">
-        Für junge und bereits etablierte Unternehmen gibt es oft unterschiedliche Förderprogramme, lass uns wissen seit wievielen Jahren es Dein Unternehmen bereits gibt und wir suchen für Dich die passenden Angebote.
+        Für junge und bereits etablierte Unternehmen gibt es oft unterschiedliche Förderprogramme. Lassen Sie uns wissen seit wievielen Jahren es Ihr Unternehmen bereits gibt und wir suchen für Sie die passenden Angebote.
       </div>
     </div>
     <button disabled={!selection.age} transition:fade|local class="next" on:click={next}>Weiter</button>
   {/if}
   {#if currentStep === 4}
     <div class="fullpage" in:fly={{ x: 1000, duration: 1500 }} out:fly={{ x: -1000, duration: 1500 }}>
-      <Select categoryName="Rechtsform" bind:value={selection.legal} options={rechtsformen} help="Je nachdem welche Rechtsform Dein Unternehmen hat wirst Du für verschiedene Fördermittel unterschiedliche Unterlagen benötigen, lass uns wissen was für eine Rechtsform Dein Unternehmen hat. Solltest Du Dir nicht sicher sein welche Rechtsform Dein Unternehmen hat kannst Du davon ausgehen, dass wenn Du alleine ein Unternehmen gegründet hast Du wahrscheinlich ein Einzelunternehmer bist. Wenn Du mit mehreren Personen ein Unternehmen gegründet hast und bisher keine Reigistierung vorgenommen hast, seid ihr wahrscheinlich eine GBR." />
+      <Select categoryName="Rechtsform" bind:value={selection.legal} options={rechtsformen} help="Je nachdem welche Rechtsform Ihr Unternehmen hat werden Sie für verschiedene Fördermittel unterschiedliche Unterlagen benötigen. Lassen Sie uns wissen was für eine Rechtsform Ihr Unternehmen hat. Sollten Sie sich nicht sicher sein welche Rechtsform Ihr Unternehmen hat, können Sie davon ausgehen, dass wenn Sie alleine ein Unternehmen gegründet haben, Sie wahrscheinlich ein Einzelunternehmer sind. Wenn Sie mit mehreren Personen ein Unternehmen gegründet haben, und bisher keine Registrierung vorgenommen haben, sind Sie wahrscheinlich eine GbR." />
     </div>
     <button disabled={!selection.legal} transition:fade|local class="next" on:click={next}>Weiter</button>
   {/if}
@@ -87,6 +87,9 @@
         Mein Unternehmen hatte 2019 einen <br>
         Umsatz von <input class="main-input" bind:value={selection.sales} style="width: 300px;" placeholder="XXXXXX" min="0" type="number" /> €
       </div>
+      <div class="help-text">
+        Je nach Umsatz Ihres Unternehmens gibt es unterschiedliche Förderprogramme, lassen Sie uns den Umsatz des letzten Jahres wissen, damit wir die für Sie passenden Programme finden können.
+      </div>
     </div>
     <button disabled={!selection.sales} transition:fade|local class="next" on:click={next}>Weiter</button>
   {/if}
@@ -95,6 +98,9 @@
       <div class="input-wrapper">
         Mein Unternehmen hat <input class="main-input" bind:value={selection.employees} style="width: 150px;" placeholder="XX" min="0" type="number" /> Mitarbeiter
       </div>
+      <div class="help-text">
+        Je nach Anzahl der Mitarbeiter in Ihrem Unternehmen gibt es unterschiedliche Förderprogramme und Hilfen, lassen Sie uns die Anzahl Ihrer Mitarbeiter wissen, damit wir die für Sie passenden Programmen finden können. Sollten Sie keine Mitarbeiter haben und ein Solo-Unternehmer:in sein, tragen Sie bitte eine "0" ein.
+      </div>
     </div>
     <button disabled={!selection.employees} transition:fade|local class="next" on:click={next}>Weiter</button>
   {/if}
@@ -102,6 +108,9 @@
     <div class="fullpage" in:fly={{ x: 1000, duration: 1500 }} out:fly={{ x: -1000, duration: 1500 }}>
       <div class="input-wrapper">
         Ich brauche Liquidität innerhalb der nächsten
+      </div>
+      <div class="help-text">
+        Es gibt Hilfsmaßnahmen für Unternehmen die akut durch die Corona-Krise bedroht sind. Sollte sich Ihr Unternehmen mit akutem Liquiditätsmangel konfrontiert sehen haben Sie möglicherweise Anspruch darauf. Bitte beachten Sie, dass dies nur für Unternehmen reserviert ist die akut bedroht sind. Sollten Sie über einen Puffer für die zunächst absehbare Zeit verfügen können Sie natürlich auch auf Fördermittel zugreifen. Bitte geben Sie uns an, wie dringlich Sie Liquidität benötigen. 
       </div>
     </div>
     <div class="next-button-wrapper" transition:fade|local>
