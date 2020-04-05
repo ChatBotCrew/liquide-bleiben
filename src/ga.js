9b444079e2f252e8d83bcd69020c59877186f35c
TimMaa <tim.maasen@ymail.com>
2020-04-05 18:58:57 +0200

    Add GTM

--- src/ga.js
+++ src/ga.js
@@ -1,6 +1,7 @@
 import { cookiesAllowed } from './store.js';
 
 const gaProperty = 'UA-161472816-2';
+const gtmProperty = 'GTM-THC2RPB';
 const disableStr = 'ga-disable-' + gaProperty;
 
 if (document.cookie.indexOf(disableStr + '=true') > -1) { window[disableStr] = true; cookiesAllowed.set(false) };
@@ -18,10 +19,17 @@ function optin() {
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  window.dataLayer = window.dataLayer || [];
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer',gtmProperty);
+
   cookiesAllowed.set(true);
   ga('create', gaProperty, 'auto');
   ga('set', 'anonymizeIp', true);
-  ga('send', 'pageview');
+  ga('require', gtmProperty)
 }
 
 cookiesAllowed.subscribe(allowed => { if(allowed) optin() });
