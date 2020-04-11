9b7660823179b5bfb67f9a53ce9e7fd5733f4873
Christof Petig <christof.petig@aptiv.com>
2020-04-11 15:26:36 +0200

    json import

--- chatbot/assets/main.js
+++ chatbot/assets/main.js
@@ -34,3 +34,22 @@ var current_buffer;
 document.getElementById('export').onclick = function() {
      save("virus.json", answers);
 }
+
+document.getElementById('import').onclick = function() {
+  var files = document.getElementById('selectFiles').files;
+  console.log(files);
+  if (files.length <= 0) {
+    return false;
+  }
+
+  var fr = new FileReader();
+
+  fr.onload = function(e) { 
+    console.log(e);
+    answers = JSON.parse(e.target.result);
+    //var formatted = JSON.stringify(result, null, 2);
+    //document.getElementById('result').value = formatted;
+  }
+  fr.readAsText(files.item(0));
+//    save("virus.json", answers);
+}
