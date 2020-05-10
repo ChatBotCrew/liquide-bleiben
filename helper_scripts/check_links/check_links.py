d3fdaf396df3b2cf912f8300b0edc71f292b9344
Lucas Humfeldt <lucas@recuirty-labs.com>
2020-05-11 00:35:21 +0200

    Made script mostly pylint conform.

--- helper_scripts/check_links/check_links.py
+++ helper_scripts/check_links/check_links.py
@@ -1,47 +1,80 @@
+"""
+This script takes a path to a file and parses
+all http/https occurences of said file to check them for
+failing http requests. Every failed request is
+listed as output to the user.
+"""
+
 from __future__ import annotations
+
+import sys
+import re
+
 from pathlib import Path
-from re import compile
 from argparse import ArgumentParser
 from http import HTTPStatus
-
-import requests 
+from collections import namedtuple
 from urllib3.exceptions import InsecureRequestWarning
 
-class Response:
-    url: str = ""
-    code: int = 0
+import requests
 
-    def __init__(self, url: str, code: int) -> Response:
-        self.url = url
-        self.code = code
+
+Response = namedtuple('Response', 'url code')
 
 def main():
 
     parser = ArgumentParser(description='Check status codes of all links found in a given file.')
-    parser.add_argument('file_path', type=Path, metavar='file', help='Path to the file to be checked')
+    parser.add_argument(
+        'file_path',
+        type=Path,
+        metavar='file',
+        help='Path to the file to be checked'
+    )
 
-    args =parser.parse_args()
+    args = parser.parse_args()
 
     # This prevents the script from spamming warnings when disabling SSL verification later.
     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
     try:
         with open(args.file_path, 'r') as data_js_file:
-            regex = compile(r'\b(?:https?|http):[\w/#~:.?+=&%@!\-.:?\\-]+?(?=[.:?\-]*(?:[^\w/#~:.?+=&%@!\-.:?\-]|$))')
+            regex = re.compile(
+                r'\b(?:https?|http):' +
+                r'[\w/#~:.?+=&%@!\-.:?\\-]+?' +
+                r'(?=[.:?\-]*(?:[^\w/#~:.?+=&%@!\-.:?\-]|$))'
+            )
             urls = regex.findall(data_js_file.read())
-    except FileNotFoundError as e:
-        print("Error: \`{0}\` does not point to a valid file.".format(args.file_path))
-        exit(1)
+    except FileNotFoundError:
+        print("Error: `{0}` does not point to a valid file.".format(args.file_path))
+        sys.exit(1)
 
     # Disabling SSL verification (`verify=False`) because this scripts purpose is to check if
     # the page is still available not if certificates or ssl versions are valid.
-    responses = [Response(url, requests.get(url, timeout=10, verify=False).status_code) for url in urls]
+    responses = [Response(
+        url,
+        requests.get(
+            url,
+            timeout=10,
+            verify=False
+        ).status_code
+    ) for url in urls]
 
     erroneous_responses = filter(lambda response: str(response.code)[0] != '2', responses)
 
-    print("The links found in `{0}` could not be accessed successfully (no success status code (2**) returned):\n".format(args.file_path))
+    print(
+        "These links found in `{0}`".format(args.file_path) +
+        " could not be accessed successfully" +
+        " (no success status code (2**) returned):\n"
+    )
+
     for  response in erroneous_responses:
-        print("    {0}:\n    {1} - {2}\n".format(response.url, response.code, HTTPStatus(response.code).phrase))
+        print(
+            "    {0}:\n    {1} - {2}\n".format(
+                response.url,
+                response.code,
+                HTTPStatus(response.code).phrase
+            )
+        )
 
 if __name__ == "__main__":
     main()
